2f7d478bbf95b7f5f9525ba38a9ff518686f4709
Rene Mayett <deathpain8@gmail.com>
2023-06-29 18:00:42 -0600

    Se concluye pagina heroe y ficha personal

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { HeroesRoutingModule } from './heroes-routing.module';
 import { HeroPageComponent } from './pages/hero-page/hero-page.component';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
@@ -8,6 +7,8 @@ import { ListPageComponent } from './pages/list-page/list-page.component';
 import { NewPageComponent } from './pages/new-page/new-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
 import { MaterialModule } from '../material/material.module';
+import { CardComponent } from './components/card/card.component';
+import { HeroImagePipe } from './pipes/hero-image.pipe';
 
 
 @NgModule({
@@ -16,7 +17,10 @@ import { MaterialModule } from '../material/material.module';
     LayoutPageComponent,
     ListPageComponent,
     NewPageComponent,
-    SearchPageComponent
+    SearchPageComponent,
+    CardComponent,
+    HeroImagePipe,
+    HeroImagePipe
   ],
   imports: [
     CommonModule,
---
+++ src/app/heroes/services/heroes.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, catchError, of } from 'rxjs';
+import { Hero } from '../interfaces/hero.interface';
+import { environments } from 'src/environments/environments';
+
+@Injectable({providedIn: 'root'})
+export class HeroesService {
+
+  private baseURL: string = environments.baseURL;
+
+  constructor(private http: HttpClient) { }
+
+  getHeroes():Observable<Hero[]>{
+    return this.http.get<Hero[]>(`${this.baseURL}/heroes`)
+  }
+
+  getHeroByID(id:string):Observable<Hero|undefined>{
+    return this.http.get<Hero>(`${this.baseURL}/heroes/${id}`).pipe(catchError(error => of(undefined)));
+  }
+
+}
+
+
